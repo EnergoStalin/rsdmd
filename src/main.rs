9b97867c81b011d0fc2ef30ab26b094aa56d5be3
EnergoStalin <ens.stalin@gmail.com>
2024-07-28 04:36:56 +0300

    refactor: all

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,90 @@
 use std::{env, fs, io::ErrorKind, path::Path, thread};
 
-use inotify::{Inotify, WatchMask};
+use inotify::{Events, Inotify, WatchMask};
 use regex::Regex;
 
 fn check_part_files<P: AsRef<Path>>(path: &P) -> bool {
-  let dir = fs::read_dir(path).unwrap();
-  for entry in dir {
-    let path = entry.unwrap().path();
-    if path.is_file() && path.extension().unwrap() == "part" {
-      return true;
-    }
-  }
+	let dir = fs::read_dir(path).unwrap();
+	for entry in dir {
+		let path = entry.unwrap().path();
+		if path.is_file() && path.extension().unwrap_or_default() == "part" {
+			return true;
+		}
+	}
 
-  return false;
+	return false;
 }
 
 fn wait_for_no_part_files<P: AsRef<Path>>(path: &P) {
-  loop {
-    if !check_part_files(path) {
-      break;
-    }
+	loop {
+		if !check_part_files(path) {
+			break;
+		}
 
-    thread::sleep(std::time::Duration::from_millis(1000));
-  }
+		thread::sleep(std::time::Duration::from_millis(1000));
+	}
 }
 
-fn main() -> ! {
-  let args: Vec<String> = env::args().collect();
+unsafe fn read_events(watcher: &mut Inotify) -> std::io::Result<Events<'static>> {
+	static mut BUFFER: &mut [u8; 1024] = &mut [0; 1024];
+	loop {
+		match watcher.read_events(BUFFER) {
+			Ok(events) => return Ok(events),
+			Err(error) if error.kind() == ErrorKind::WouldBlock => continue,
+			Err(error) => return Err(error),
+		};
+	};
+}
 
-  let watch_path = Path::new(args.get(1).expect("No path specified"));
-  let move_to = Path::new(args.get(2).expect("No path move to specified"));
-  let pattern_string = args.get(3).expect("No pattern specified");
+fn main() {
+	let args: Vec<String> = env::args().collect();
 
-  let pattern = Regex::new(pattern_string).expect("Invalid pattern");
+	let watch_path = Path::new(args.get(1).expect("No source path specified"));
+	let move_to = Path::new(args.get(2).expect("No destination path specified"));
+	let pattern_string = args.get(3).expect("No file pattern specified");
 
-  if !move_to.exists() {
-    panic!("Move to path does not exist");
-  }
+	let pattern = Regex::new(pattern_string).expect("Invalid regex expression");
 
-  if !watch_path.exists() {
-    panic!("Watch path does not exist");
-  }
+	if !move_to.exists() {
+		panic!("Destination path does not exist");
+	}
 
-  let mut watcher = Inotify::init().expect("Error while initializing directory watcher instance");
+	if !watch_path.exists() {
+		panic!("Watch path does not exist")
+	}
 
-  watcher
-    .watches()
-    .add(watch_path, WatchMask::CREATE)
-    .expect("Failed to add file watch");
+	let mut watcher = Inotify::init().expect("Error while initializing directory watcher instance");
 
-  let mut buffer = [0; 1024];
-  loop {
-    let directory_events = loop {
-      match watcher.read_events(&mut buffer) {
-        Ok(events) => break events,
-        Err(error) if error.kind() == ErrorKind::WouldBlock => continue,
-        _ => panic!("Failed to read events"),
-      }
-    };
+	watcher
+		.watches()
+		.add(watch_path, WatchMask::CREATE)
+		.expect("Failed adding file to watch list");
 
-    for event in directory_events {
-      let path = watch_path.join(match event.name {
-        Some(name) => name,
-        None => continue,
-      });
+	loop {
+		let directory_events = unsafe { read_events(&mut watcher) };
 
-      if !pattern.is_match(path.to_str().unwrap()) {
-        continue;
-      }
+		for event in directory_events.unwrap() {
+			let path = watch_path.join(match event.name {
+				Some(name) => name,
+				None => continue,
+			});
 
-      println!("Matched: {}", path.display());
+			if !pattern.is_match(path.to_str().unwrap()) {
+				continue;
+			}
 
-      let name = path.file_name().unwrap();
-      let new = Path::new(move_to).join(name);
+			println!("Matched: {}", path.display());
 
-      println!("Waiting {} to download.", path.display());
+			let name = path.file_name().unwrap();
+			let new = Path::new(move_to).join(name);
 
-      wait_for_no_part_files(&watch_path);
+			println!("Waiting {} to download.", path.display());
 
-      println!("Moving {} to {}", path.display(), move_to.display());
+			wait_for_no_part_files(&watch_path);
 
-      fs::rename(path, new).expect("Failed to move file");
-    }
-  }
+			println!("Moving {} to {}", path.display(), move_to.display());
+
+			fs::rename(path, new).expect("Failed to move file");
+		}
+	}
 }
